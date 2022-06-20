cbc6041f2d8caf4bca87d2047b443d9d197053a7
AMR Solutions LTD <amrorg333@gmail.com>
2022-06-20 19:00:37 +0530

    Created hasher utility

---
+++ hasher.py
@@ -0,0 +1,11 @@
+from passlib.context import CryptContext
+
+class Hasher():
+  def __init__(self):
+    self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+  def verify_password(self, plain_password, hashed_password):
+    return self.pwd_context.verify_password(hashed_password, plain_password)
+
+  def get_hashed_password(self, plain_password):
+    return self.pwd_context.hash(plain_password)
