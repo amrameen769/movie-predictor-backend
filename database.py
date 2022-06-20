2dc556c038d13e76898d77ef7e6616b9cbf8c9d9
AMR Solutions LTD <amrorg333@gmail.com>
2022-06-20 18:46:04 +0530

    ObjectId Overrider Updated

--- database.py
+++ database.py
@@ -20,5 +20,24 @@ class MotorDB():
     else: return False
   
 
+
+# ObjectID Override
+
+from bson import ObjectId
+
+class PyObjectId(ObjectId):
+  @classmethod
+  def __get_validators__(self):
+    yield self.validate
   
+  def validate(self, value):
+    if not ObjectId.is_valid(value):
+      raise ValueError("Invalid ObjectID")
+    
+    return ObjectId(value)
+  
+  @classmethod
+  def __modify_schema__(self, field_schema):
+    field_schema.update(type="string")
+
 
