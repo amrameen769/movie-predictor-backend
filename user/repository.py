9d3b7bb17620165ccbd4b3d28319d4763f356940
suhaibvazin <suhaibvazin321@gmail.com>
2022-06-22 21:37:41 +0530

    Reviewed and Updated changes

--- user/repository.py
+++ user/repository.py
@@ -21,19 +21,19 @@ async def create_user(user: UserSchema.User):
     await motor.connect_db(db_name="movie_predictor")
     user_col = await motor.get_collection(col_name="user")
 
-    user["password"] = hasher.get_hashed_password(user["password"])
-
     existing_user = await check_existing_user(
         username=user["username"], email=user["email"]
     )
 
     if not existing_user:
+        user["password"] = hasher.get_hashed_password(user["password"])
+        # hashing is only required if new user data is not existing
         new_user = await user_col.insert_one(user)
 
         if not new_user:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                detail="User Creation",
+                detail="User Creation failed", # corrected typo
             )
         else:
             created_user = await user_col.find_one({"_id": new_user.inserted_id})
